bcd519112b1f62c7b18f11571fd20aa857849606
Steven <116656099+StevenK-293@users.noreply.github.com>
2024-06-20 18:35:45 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -7,22 +7,28 @@ const rawScriptTable = {
     'test1': 'https://raw.githubusercontent.com/StevenK-293/rawscript/main/raw/scripts/main_1.lua',
 };
 
+// Styled welcome message
+const welcomeMessage = `
+************************************************
+*                                              *
+*    Welcome to the rawscript API endpoint     *
+*                                              *
+************************************************
+
+Usage:
+- /api/raw/<script_name>: Fetches the script content.
+
+Available Scripts:
+- test1: Retrieves main_1.lua script from GitHub.
+`;
+
 app.get('/', (req, res) => {
     res.send('Welcome to the home page.');
 });
 
 app.get('/api', (req, res) => {
-    const instructions = `
-    Welcome to the API endpoint.
-    
-    Usage:
-    - /api/raw/<script_name>: Fetches the content of the script.
-    
-    Available Scripts:
-    - test1: Retrieves main_1.lua script from GitHub.
-    `;
     res.set('Content-Type', 'text/plain');
-    res.send(instructions);
+    res.send(welcomeMessage);
 });
 
 app.get('/api/raw/:script_name', async (req, res) => {
@@ -40,8 +46,18 @@ app.get('/api/raw/:script_name', async (req, res) => {
         res.set('Content-Type', 'text/plain');
         res.send(response.data);
     } catch (error) {
-        res.status(error.response ? error.response.status : 500).send('failed to fetch script');
+        console.error('Failed to fetch script:', error);
+        res.status(error.response ? error.response.status : 500).send('Failed to fetch script');
     }
+});
+
+app.get('*', (req, res) => {
+    res.status(404).send('Not the right endpoint');
+});
+
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
 });
 
 module.exports = app;
